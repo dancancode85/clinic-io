62e6d89073f8e0a3760fde8fa7b58109ca153c8d
dannygorgon <thirdoptionisthename@googlemail.com>
2025-12-01 20:44:13 +0000

    feat(API): add Express server. add get endpoint with log. add port logging.

---
+++ apps/api/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.json({ status: "ok", message: "API is running" , timestamp: new Date().toISOString() });
+});
+
+app.listen(port, () => {
+    console.log(`Up and running on port ${port}`);
+});
